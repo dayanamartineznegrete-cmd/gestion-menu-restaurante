dc5a8c9c1d7a885127dd7abd9faa68f7221871e4
Dayana Martinez <dayanamartineznegrete@gmail.com>
2026-05-21 23:00:24 -0500

    subo primera versión del menú con estructura inicial

--- menu_restaurante.py
+++ menu_restaurante.py
@@ -14,3 +14,28 @@ print("\nMENÚ DEL RESTAURANTE")
 
 for producto in menu:
     print(producto)
+    print("\nMENÚ DEL RESTAURANTE")
+print("----------------------")
+
+for producto in menu:
+    print("Producto:", producto[0])
+    print("Categoría:", producto[1])
+    print("Precio Base:", producto[2])
+    print("----------------------")
+    # Variables de promoción
+
+categoria_objetivo = "Comida rápida"
+umbral_precio = 15000
+descuento = 15
+print("\nPROMOCIONES")
+print("----------------------")
+
+for producto in menu:
+
+    if producto[1] == categoria_objetivo:
+        nuevo_precio = producto[2] - 15
+
+    else:
+        nuevo_precio = producto[2]
+
+    print(producto[0], nuevo_precio)
