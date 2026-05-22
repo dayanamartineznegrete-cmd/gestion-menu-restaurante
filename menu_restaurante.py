24458a6b7ad6fb37034be53b208283a8b40c423b
Dayana Martinez <dayanamartineznegrete@gmail.com>
2026-05-21 22:07:54 -0500

    Creación inicial de la matriz del menú

---
+++ menu_restaurante.py
@@ -0,0 +1,16 @@
+# Menú del restaurante
+
+menu = [
+    ["Hamburguesa", "Comida rápida", 18000],
+    ["Pizza", "Comida rápida", 25000],
+    ["Jugo Natural", "Bebida", 8000],
+    ["Café Latte", "Bebida", 7000],
+    ["Cheesecake", "Postre", 12000],
+    ["Ensalada César", "Saludable", 15000]
+]
+
+print(menu)
+print("\nMENÚ DEL RESTAURANTE")
+
+for producto in menu:
+    print(producto)
